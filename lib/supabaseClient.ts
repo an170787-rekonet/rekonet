e1402be050bbbe17b1589d495cf25cd9f43b1031
an170787-rekonet <an170787@hotmail.com>
2026-02-20 13:38:43 +0000

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
-
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-
-export const supabase = createClient(url, anon)
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
