b3a9c0d9a8d483c66cc9a500f20da5cf4f2a25b8
an170787-rekonet <an170787@hotmail.com>
2026-02-22 23:33:27 +0000

    Create page.jsx

---
+++ app/assessment/cv-upload/page.jsx
@@ -0,0 +1,14 @@
+import { Suspense } from 'react';
+import ClientCvUpload from './ClientCvUpload';
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function Page() {
+  return (
+    <Suspense fallback={<main style={{ padding: 24 }}>Loading…</main>}>
+      <ClientCvUpload />
+    </Suspense>
+  );
+}
+``
