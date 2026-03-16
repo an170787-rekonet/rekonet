ec9c54090da9bd55f0b1ab94e7737fa6ae3ceed0
an170787-rekonet <an170787@hotmail.com>
2026-03-16 20:49:35 +0000

    Update page.jsx

--- app/assessment/page.jsx
+++ app/assessment/page.jsx
@@ -1,8 +1,6 @@
 // app/assessment/page.jsx
 import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
-
 export default function Page() {
-  // Redirect users to the real first step of the assessment
   redirect("/assessment/language");
 }
